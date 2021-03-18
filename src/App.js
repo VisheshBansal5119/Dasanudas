b2221418c1fae9fb800b0fdfe3cc86e9b93a263a
Vishesh0112 <visheshbansal369@gmail.com>
2021-03-18 00:11:44 -0400

    adding signin functionality

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
 import React,{Component} from 'react';
-import Nav from "./NavigationBar";
 import {members} from './members';
 import CardList from './cardlist';
-// const fs = require('fs');
+import Signin from './Signin/Signin';
+import './App.css';
 
-// fs.readFile('./test.txt',{ encoding: 'utf-8'},(err,data)=>{
-//   console.log(data);
-// });
 class App extends Component{
- 
+ constructor(){
+   super(
+
+   );
+   this.state = {
+     route: 'signin',
+     vatsal: 'false',
+     vishesh: 'false'
+   }
+ }
+onRouteChange =() =>{
+  this.setState({route: 'home'});
+}
+
   render(){
     return(
 
       <div>
-        <Nav/>
+        
         <div id="colorstrip"/>
         <h1 id="Main-heading">Dasanudas</h1>
+        { this.state.route === 'signin' ? <Signin onRouteChange={this.onRouteChange}/>
+        :<div>
+       
+        <h2 id="sub-heading">Yuvako</h2>
         <div id="strip">
-         <span> <marquee style={{fontSize: '25px' ,padding: '5'  , color: 'white'}}>The journey of a thousand miles begins with one step.</marquee>
+         <span > <marquee style={{fontSize: '25px' ,padding: '5'  , color: 'white'}}>The journey of a thousand miles begins with one step.</marquee>
          </span>
         </div>
-        <h2 id="sub-heading">Yuvako</h2>
         <CardList members={members}/>
         <span id="credit">Created by Vishesh Bansal</span>
-        {
-          localStorage.setItem("name","Vishesh")
-          
-        }
+        </div>
+  }
       </div>
     );
   }
--- src/card.jsx
+++ src/card.jsx
@@ -8,7 +8,7 @@ const Card =({photo,name,id,email}) => {
     return (
 
         <div className='bg-purple dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <a href={email}><img  className="image-overlay" alt='Yuvak' src={photo} width="300" height="300"/></a>
+            <a href={email}><img alt='Yuvak' src={photo} width="300" height="300"/></a>
             <div>
                 
                 <h2>{name}</h2>
