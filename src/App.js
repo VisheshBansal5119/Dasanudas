c163ae2f7b112f7bc2c88077823a584bc9c77733
Vishesh0112 <visheshbansal369@gmail.com>
2021-03-20 02:06:27 -0400

    register page added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Signin from './Signin/Signin';
 import './App.css';
 import './index.css';
 import Nav from './NavigationBar';
+import Register from './Register';
 class App extends Component{
  constructor(){
    super(
@@ -30,22 +31,27 @@ onDouble =()=>{
         <div id="colorstrip"/>
         <Nav onRouteChange={this.onRouteChange}/>
         <h1 id="Main-heading">Dasanudas</h1>
-        { this.state.route === 'signin' ? <Signin onRouteChange={this.onRouteChange} onDouble={this.onDouble}/>
-        :<div>
-       
-        <h2 id="sub-heading">Yuvako</h2>
-        <div id="strip">
-        
-         <span > 
-        <marquee style={{color : 'white', fontSize: '25px'}} width="100%" direction="left" height="100px">
-         Always forgive your enemies; nothing annoys them so much.
-        </marquee>
-         </span>
-        </div>
-        
-        <CardList members={members}/>
-        <span id="credit">Created by Vishesh Bansal</span>
-        </div>
+        { this.state.route === 'home' 
+          ?<div>
+           <h2 id="sub-heading">Yuvako</h2>
+            <div id="strip">
+            
+            <span > 
+            <marquee style={{color : 'white', fontSize: '25px'}} width="100%" direction="left" height="100px">
+            Always forgive your enemies; nothing annoys them so much.
+            </marquee>
+            </span>
+            </div>
+            
+            <CardList members={members}/>
+            <span id="credit">Created by Vishesh Bansal</span>
+            </div>
+         
+        :(
+          this.state.route === "signin"
+          ?<Signin onRouteChange={this.onRouteChange} />
+          :<Register onRouteChange={this.onRouteChange} />
+        )
         }
       </div>
     );
--- src/Register.jsx
+++ src/Register.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import './Signin.css';
-const Signin = ({onRouteChange, onDouble}) =>{
+const Register = ({onRouteChange, onDouble}) =>{
     
     
         return (
@@ -10,9 +9,17 @@ const Signin = ({onRouteChange, onDouble}) =>{
                <main class="pa4 black-80">
                     <form class="measure center">
                         <fieldset id="sign_up" class="ba b--transparent ph0 mh0">
-                        <legend class="f4 fw6 ph0 mh0 center bold white">Sign In</legend>
+                        <legend class="f4 fw6 ph0 mh0 center  ">Register </legend>
                         <div class="mt3">
-                            <label class="db fw6 lh-copy f6" for="email-address">Username</label>
+                            <label class="db fw6 lh-copy f6" for="text">Username</label>
+                            <input class="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
+                        </div>
+                        <div class="mt3">
+                            <label class="db fw6 lh-copy f6" for="email-address">Email</label>
+                            <input class="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
+                        </div>
+                        <div class="mt3">
+                            <label class="db fw6 lh-copy f6" for="email-address">Confirm Email</label>
                             <input class="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
                         </div>
                         <div class="mv3">
@@ -26,11 +33,7 @@ const Signin = ({onRouteChange, onDouble}) =>{
                         onClick={()=>onRouteChange("home")}
                         
                         class="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
-                        type="submit" value="Sign in"/>
-                        </div>
-                        <div class="lh-copy mt3">
-                        <p class="f6 link dim black db pointer " onClick={()=>onRouteChange("register")} >Register Now</p>
-                        
+                        type="submit" value="Register"/>
                         </div>
                     </form>
             </main>
@@ -40,4 +43,4 @@ const Signin = ({onRouteChange, onDouble}) =>{
     }
 
  
-export default Signin;
+export default Register;
