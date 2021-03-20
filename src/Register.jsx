2b237504d1ea68c7ed9dfec88f803ba12cf67235
Vishesh0112 <visheshbansal369@gmail.com>
2021-03-20 13:54:57 -0400

    unknown

--- src/Register.jsx
+++ src/Register.jsx
@@ -11,19 +11,19 @@ const Register = ({onRouteChange}) =>{
                         <fieldset id="sign_up" class="ba b--transparent ph0 mh0">
                         <legend class="f4 fw6 ph0 mh0 center  ">Register </legend>
                         <div class="mt3">
-                            <label class="db fw6 lh-copy f6" for="text">Username</label>
+                            <label class="db fw6 lh-copy f6" htmlFor="text">Username</label>
                             <input class="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
                         </div>
                         <div class="mt3">
-                            <label class="db fw6 lh-copy f6" for="email-address">Email</label>
+                            <label class="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                             <input class="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
                         </div>
                         <div class="mt3">
-                            <label class="db fw6 lh-copy f6" for="email-address">Confirm Email</label>
+                            <label class="db fw6 lh-copy f6" htmlFor="email-address">Confirm Email</label>
                             <input class="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
                         </div>
                         <div class="mv3">
-                            <label class="db fw6 lh-copy f6" for="password">Password</label>
+                            <label class="db fw6 lh-copy f6" htmlFor="password">Password</label>
                             <input  class="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password"/>
                         </div>
                         </fieldset>
--- src/Signin/Signin.jsx
+++ src/Signin/Signin.jsx
@@ -12,11 +12,11 @@ const Signin = ({onRouteChange, onDouble}) =>{
                         <fieldset id="sign_up" class="ba b--transparent ph0 mh0">
                         <legend class="f4 fw6 ph0 mh0 center bold">Sign In</legend>
                         <div class="mt3">
-                            <label class="db fw6 lh-copy f6" for="email-address">Username</label>
+                            <label class="db fw6 lh-copy f6" htmlFor="email-address">Username</label>
                             <input class="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
                         </div>
                         <div class="mv3">
-                            <label class="db fw6 lh-copy f6" for="password">Password</label>
+                            <label class="db fw6 lh-copy f6" htmlFor="password">Password</label>
                             <input onClick={onDouble} class="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password"/>
                         </div>
                         </fieldset>
