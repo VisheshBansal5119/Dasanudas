ed7d490f8dac949c601ae5b477b8b03a815ad77e
Vishesh0112 <visheshbansal369@gmail.com>
2021-04-29 12:51:20 -0400

    Re-routing register form

--- src/Register.jsx
+++ src/Register.jsx
@@ -30,7 +30,7 @@ const Register = ({onRouteChange}) =>{
                         <div class="">
                         <input 
                         
-                        onClick={()=>onRouteChange("home")}
+                        onClick={()=>onRouteChange("signin")}
                         
                         class="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
                         type="submit" value="Register"/>
